60e869eedf2793c8b7b3bb2be1b9d76f997e731b
Maurício Santana <69904827+m9sant@users.noreply.github.com>
2020-09-24 16:14:58 -0300

    Create exercicio03.py

---
+++ LTP1-2020/Pratica03/exercicio03.py
@@ -0,0 +1,17 @@
+a = float(input('Coeficiente A:'))
+b = float(input('Coeficiente B:'))
+c = float(input('Coeficiente C:'))
+
+#Potencia no python- x elevado a y == x**y
+delta = (b**2) - (4*a*c)
+
+#Determina o número de raiz em função do valor do delta
+if delta < 0:
+  print('Nao possui raizes reais')
+elif delta > 0:
+  x1 = (-b + delta**0.5) / (2*a)
+  x2 = (-b - delta**0.5) / (2*a)
+  print('Raizes:', x1, x2)
+else:
+  x2 = (-b - delta**0.5) / (2*a)
+  print('Raiz:', x2)
