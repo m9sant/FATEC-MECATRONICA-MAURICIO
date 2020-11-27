e1bb382e87e57195ab2a1fbdb805ac80cd91ce65
Maurício Santana <69904827+m9sant@users.noreply.github.com>
2020-11-27 15:43:01 -0300

    Create prog02.py

---
+++ LTP1-2020/Pratica08/prog02.py
@@ -0,0 +1,10 @@
+palavra = "Kakaroto Goku"
+letra = palavra[0]
+
+#CUIDADO: não pode acessar uma posição que não existe
+#letra = palavra[1000]
+
+letras = palavra[1:5]
+letras = palavra[5:]
+letras = palavra[:8]
+print(letras)
