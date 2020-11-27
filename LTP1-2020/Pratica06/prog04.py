50b70e6750a31079a7981e2002a91052ea7dfa11
Maurício Santana <69904827+m9sant@users.noreply.github.com>
2020-11-27 14:45:26 -0300

    Create prog04.py

---
+++ LTP1-2020/Pratica06/prog04.py
@@ -0,0 +1,10 @@
+temperatura_limite = float(input("Informe uma temperatura limite:"))
+temperatura_atual = float(input("Informe a temperatura atual:"))
+
+if temperatura_atual > temperatura_limite:
+  print("Temperatural atual acima do limite!")
+
+if temperatura_atual < temperatura_limite:
+  print('Temperatura atual abaixo do limite!')
+
+print('Obrigado por utilizar o sistema!')
