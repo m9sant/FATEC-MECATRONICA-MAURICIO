62dfcec0a0a08eaea4c22e49a3813906ce488154
Maurício Santana <69904827+m9sant@users.noreply.github.com>
2021-04-06 17:28:48 -0300

    Update Q2.ino

--- Microcrontroladores_2021/Sim_P1/Q2.ino
+++ Microcrontroladores_2021/Sim_P1/Q2.ino
@@ -1,1 +1,26 @@
+#define BOT_ON 0
+#define BOT_OFF 1
+#define LED_ON 0
+#define LED_OFF 1
+#define BOTAO 5
+#define LED 12
 
+void inicializa_hardware() {
+  pinMode(LED, OUTPUT);
+  pinMode(BOTAO, INPUT_PULLUP);
+  digitalWrite(LED, LED_OFF);
+}
+
+void setup() {
+  inicializa_hardware();
+}
+
+void loop() {
+  if(digitalRead(BOTAO) == (BOT_ON) {
+    digitalWrite(LED, LED_ON);
+  }
+     
+  if(digitalRead(BOTAO) == (BOT_OFF) {
+    digitalWrite(LED, LED_OFF);
+     }
+        }
